e0423cb2eae26b2fef59548336517f77e5f65949
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2025-04-07 07:52:33 +0530

    commit frontend

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import AddCourse from './pages/educator/AddCourse'
 import MyCourse from './pages/educator/MyCourse'
 import StudentEnrolled from './pages/educator/StudentEnrolled'
 import { Navbar } from './components/student/Navbar'
+import "quill/dist/quill.snow.css";
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
@@ -19,6 +21,7 @@ const App = () => {
 
   return (
     <div className='text-default min-h-screen bg-white'>
+      <ToastContainer />
       {!isEducatorRoute && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />}></Route>
@@ -29,9 +32,9 @@ const App = () => {
         <Route path='/player/:courseId' element={<Player />}></Route>
         <Route path='/loading/:path' element={<Loading />}></Route>
         <Route path='/educator' element={<Educator />}>
-            <Route path='educator' element={<Dashboard />} />
+            <Route path='/educator' element={<Dashboard />} />
             <Route path='add-course' element={<AddCourse />} />
-            <Route path='my-course' element={<MyCourse />} />
+            <Route path='my-courses' element={<MyCourse />} />
             <Route path='student-enrolled' element={<StudentEnrolled />} />
         </Route>
       </Routes>
